6f244217db65500edb7ab28fce34574dc17963f1
wushuxiong <18779884209@163.com>
2018-09-18 18:27:32 +0800

    分享优化

--- WeexNativeSupport/Manage/WeexNativeSupportManage.m
+++ WeexNativeSupport/Manage/WeexNativeSupportManage.m
@@ -10,10 +10,12 @@
 #import "CMBaseService.h"
 #import "MJShareItem.h"
 #import "UIImage+ScaleImage.h"
+#import <SDWebImageManager.h>
+#import <SVProgressHUD.h>
 @interface WeexNativeSupportManage ()
-    
+
 @property (nonatomic, copy) NSString *url;  //存储域名
-    
+
 @end
 
 @implementation WeexNativeSupportManage
@@ -26,7 +28,7 @@ static WeexNativeSupportManage *manager = nil;
     return manager;
     
 }
-#pragma mark -- 检查更新
+#pragma mark --检查更新
 - (void)checkVersionToUpdateWithUrl:(NSString *)url appId:(NSString *)appId isShowLatestVersionTips:(BOOL)isShow{
     if (url) {
         _url = url;
@@ -64,59 +66,69 @@ static WeexNativeSupportManage *manager = nil;
     [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:alertCtrl animated:YES completion:nil];
 }
 
-#pragma mark -- 分享图片
+#pragma mark --分享图片
 - (void)activityShareWithImageUrlArray:(NSArray *)urlArray{
     
     NSMutableArray *mArray = [[NSMutableArray alloc] init];
+    [SVProgressHUD show];
     
-    
-    int i = 0;
+    __block int i = 0;
     for (NSString *urlString  in urlArray) {
         
-        NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:urlString]];
-        UIImage *image = [UIImage imageWithData:data]; // 取得图片
+        //        NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:urlString]];
+        //        UIImage *image = [UIImage imageWithData:data]; // 取得图片
         
-        //防止太大分享失败,目前微信限制最大是32kb
-        [UIImage scaleImage:image toKb:32];
+        SDWebImageManager *manager = [SDWebImageManager sharedManager] ;
+        [manager downloadImageWithURL:[NSURL URLWithString:urlString] options:0 progress:^(NSInteger   receivedSize, NSInteger expectedSize) {
+            // progression tracking code
+        }  completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType,   BOOL finished, NSURL *imageURL) {
+            if (image) {
+                //防止太大分享失败,目前微信限制最大是32kb
+                [UIImage scaleImage:image toKb:32];
+                
+                //本地路径
+                NSString *str = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES).firstObject;
+                NSString *filePath = [NSString stringWithFormat:@"%@/huaji%d.jpg",str,i];
+                //保存本地
+                [UIImagePNGRepresentation(image) writeToFile:filePath atomically:YES];
+                
+                NSURL *fileURL = [NSURL fileURLWithPath:filePath];
+                
+                //把图片和路径传进来
+                MJShareItem *item = [[MJShareItem alloc] initWithImage:image andfile:fileURL];
+                [mArray addObject:item];
+                i++;
+                if (i == urlArray.count - 1) {
+                    [SVProgressHUD dismiss];
+                    //里面initWithActivityItems  传的是item的数组  如果直接用图片数组的话 会经常出现 微信断开的错误
+                    UIActivityViewController *activityView =[[UIActivityViewController alloc] initWithActivityItems:mArray
+                                                                                              applicationActivities:nil];
+                    
+                    //需要忽略的分享
+                    activityView.excludedActivityTypes = @[
+                                                           UIActivityTypePrint,
+                                                           UIActivityTypeCopyToPasteboard,
+                                                           UIActivityTypeAssignToContact,
+                                                           UIActivityTypeSaveToCameraRoll,
+                                                           UIActivityTypeMail,
+                                                           UIActivityTypePrint,
+                                                           UIActivityTypeCopyToPasteboard,
+                                                           UIActivityTypeAssignToContact,
+                                                           UIActivityTypeSaveToCameraRoll,
+                                                           UIActivityTypeAddToReadingList,
+                                                           UIActivityTypePostToFlickr,
+                                                           UIActivityTypeAirDrop
+                                                           ];
+                    
+                    activityView.restorationIdentifier = @"activity";
+                    [activityView setTitle:@"分享"];
+                    [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:activityView animated:TRUE completion:nil];
+                }
+            }
+        }];
         
-        //本地路径
-        NSString *str = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES).firstObject;
-        NSString *filePath = [NSString stringWithFormat:@"%@/huaji%d.jpg",str,i];
-        //保存本地
-        [UIImagePNGRepresentation(image) writeToFile:filePath atomically:YES];
         
-        NSURL *fileURL = [NSURL fileURLWithPath:filePath];
-        
-        //把图片和路径传进来
-        MJShareItem *item = [[MJShareItem alloc] initWithImage:image andfile:fileURL];
-        [mArray addObject:item];
-        i++;
     }
-    
-    //里面initWithActivityItems  传的是item的数组  如果直接用图片数组的话 会经常出现 微信断开的错误
-    UIActivityViewController *activityView =[[UIActivityViewController alloc] initWithActivityItems:mArray
-                                                                              applicationActivities:nil];
-    
-    //需要忽略的分享
-    activityView.excludedActivityTypes = @[
-                                           UIActivityTypePrint,
-                                           UIActivityTypeCopyToPasteboard,
-                                           UIActivityTypeAssignToContact,
-                                           UIActivityTypeSaveToCameraRoll,
-                                           UIActivityTypeMail,
-                                           UIActivityTypePrint,
-                                           UIActivityTypeCopyToPasteboard,
-                                           UIActivityTypeAssignToContact,
-                                           UIActivityTypeSaveToCameraRoll,
-                                           UIActivityTypeAddToReadingList,
-                                           UIActivityTypePostToFlickr,
-                                           UIActivityTypeAirDrop
-                                           ];
-    
-    activityView.restorationIdentifier = @"activity";
-    [activityView setTitle:@"分享"];
-    [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:activityView animated:TRUE completion:nil];
-    
 }
 
 @end
