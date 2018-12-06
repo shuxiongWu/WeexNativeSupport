c5cdc5571c55e10133332f443eb58dfa4b34d5bc
吴述雄 <18779884209@163.com>
2018-12-06 15:07:32 +0800

    tag1.3.7

--- WeexNativeSupport.podspec
+++ WeexNativeSupport.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
 s.name         = 'WeexNativeSupport'
-s.version      = '1.3.6'
+s.version      = '1.3.7'
 s.summary      = 'weex原生支持'
 s.homepage     = 'https://github.com/shuxiongWu/WeexNativeSupport.git'
 s.license      = { :type => "MIT", :file => "LICENSE" }
--- WeexNativeSupport/Module/CMMapModule.m
+++ WeexNativeSupport/Module/CMMapModule.m
@@ -7,41 +7,41 @@
 //
 
 #import "CMMapModule.h"
-#import "MapViewCtl.h"
-#import <WeexSDK/WeexSDK.h>
-@interface CMMapModule ()
-@property (nonatomic, copy) WXModuleKeepAliveCallback locationCallBack;
-@property (nonatomic, strong) MapViewCtl *mapCtl;
-@end
+//#import "MapViewCtl.h"
+//#import <WeexSDK/WeexSDK.h>
+//@interface CMMapModule ()
+//@property (nonatomic, copy) WXModuleKeepAliveCallback locationCallBack;
+//@property (nonatomic, strong) MapViewCtl *mapCtl;
+//@end
+//
+//@implementation CMMapModule
+//@synthesize weexInstance;
+////地图定位页面
+//WX_EXPORT_METHOD(@selector(pushToCtrlGetLocation:))
+//
+//+ (void)load{
+//    [WXSDKEngine registerModule:@"CMMapModule" withClass:[CMMapModule class]];
+//}
+//
+//#pragma mark -- 地图定位
+//- (void)pushToCtrlGetLocation:(WXModuleKeepAliveCallback)callBack{
+//    self.locationCallBack = callBack;
+//   // [weexInstance.viewController.navigationController setNavigationBarHidden:NO animated:YES];
+//    [weexInstance.viewController.navigationController pushViewController:self.mapCtl animated:YES];
+//
+//    //[[[UIApplication sharedApplication].keyWindow.rootViewController.childViewControllers firstObject] presentViewController:self.mapCtl animated:YES completion:nil];
+//}
+//
+//- (MapViewCtl *)mapCtl
+//{
+//    if (!_mapCtl) {
+//        _mapCtl = [[MapViewCtl alloc] initWithNibName:NSStringFromClass([MapViewCtl class]) bundle:nil];
+//        __weak typeof(self)weakSelf = self;
+//        _mapCtl.locationAddressBlk = ^(double longitude, double latitude, NSString *address, NSString *detailAddress) {
+//            weakSelf.locationCallBack ? weakSelf.locationCallBack(@{@"longitude": @(longitude), @"latitude": @(latitude), @"address": address, @"detailAddress": detailAddress}, YES) : nil;
+//        };
+//    }
+//    return _mapCtl;
+//}
 
-@implementation CMMapModule
-@synthesize weexInstance;
-//地图定位页面
-WX_EXPORT_METHOD(@selector(pushToCtrlGetLocation:))
-
-+ (void)load{
-    [WXSDKEngine registerModule:@"CMMapModule" withClass:[CMMapModule class]];
-}
-
-#pragma mark -- 地图定位
-- (void)pushToCtrlGetLocation:(WXModuleKeepAliveCallback)callBack{
-    self.locationCallBack = callBack;
-   // [weexInstance.viewController.navigationController setNavigationBarHidden:NO animated:YES];
-    [weexInstance.viewController.navigationController pushViewController:self.mapCtl animated:YES];
-    
-    //[[[UIApplication sharedApplication].keyWindow.rootViewController.childViewControllers firstObject] presentViewController:self.mapCtl animated:YES completion:nil];
-}
-
-- (MapViewCtl *)mapCtl
-{
-    if (!_mapCtl) {
-        _mapCtl = [[MapViewCtl alloc] initWithNibName:NSStringFromClass([MapViewCtl class]) bundle:nil];
-        __weak typeof(self)weakSelf = self;
-        _mapCtl.locationAddressBlk = ^(double longitude, double latitude, NSString *address, NSString *detailAddress) {
-            weakSelf.locationCallBack ? weakSelf.locationCallBack(@{@"longitude": @(longitude), @"latitude": @(latitude), @"address": address, @"detailAddress": detailAddress}, YES) : nil;
-        };
-    }
-    return _mapCtl;
-}
-
-@end
+//@end
