5ab36e71c7c29f748f4c3181a364fee5cc6674bb
陈燕军 <229103728@qq.com>
2019-05-22 10:01:37 +0800

    【v1.4.61】修复选择照片完成之后dismiss错误的bug

--- WeexNativeSupport/Manage/WeexNativeSupportManage.m
+++ WeexNativeSupport/Manage/WeexNativeSupportManage.m
@@ -908,7 +908,7 @@ static AFHTTPSessionManager *netWorkManager;
                 }
                 
                 self.imageCallBack ? self.imageCallBack([base64StringArr mj_JSONString], YES) : nil;
-                [[self getCurrentVC] dismissViewControllerAnimated:YES completion:nil];
+                [viewController dismissViewControllerAnimated:YES completion:nil];
             } failed:^{
                 
             }];
@@ -1000,45 +1000,5 @@ static AFHTTPSessionManager *netWorkManager;
     return _imagePickerCtl;
 }
 
-#pragma mark 获取当前屏幕显示的viewcontroller
-- (UIViewController *)getCurrentVC
-{
-    // 定义一个变量存放当前屏幕显示的viewcontroller
-    UIViewController *result = nil;
-    
-    // 得到当前应用程序的主要窗口
-    UIWindow * window = [[UIApplication sharedApplication] keyWindow];
-    
-    // windowLevel是在 Z轴 方向上的窗口位置，默认值为UIWindowLevelNormal
-    if (window.windowLevel != UIWindowLevelNormal)
-    {
-        // 获取应用程序所有的窗口
-        NSArray *windows = [[UIApplication sharedApplication] windows];
-        for(UIWindow * tmpWin in windows)
-        {
-            // 找到程序的默认窗口（正在显示的窗口）
-            if (tmpWin.windowLevel == UIWindowLevelNormal)
-            {
-                // 将关键窗口赋值为默认窗口
-                window = tmpWin;
-                break;
-            }
-        }
-    }
-    
-    // 获取窗口的当前显示视图
-    UIView *frontView = [[window subviews] objectAtIndex:0];
-    
-    // 获取视图的下一个响应者，UIView视图调用这个方法的返回值为UIViewController或它的父视图
-    id nextResponder = [frontView nextResponder];
-    
-    // 判断显示视图的下一个响应者是否为一个UIViewController的类对象
-    if ([nextResponder isKindOfClass:[UIViewController class]]) {
-        result = nextResponder;
-    } else {
-        result = window.rootViewController;
-    }
-    return result;
-}
 
 @end
